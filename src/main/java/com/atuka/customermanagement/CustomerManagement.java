3875371c50bcca48406f58e0d759ff07ef321be9
ataklti <atuka2@gmail.com>
2024-04-01 16:08:21 +0300

    initial Project that commited with out any Change

---
+++ src/main/java/com/atuka/customermanagement/CustomerManagement.java
@@ -0,0 +1,20 @@
+package com.atuka.customermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CustomerManagement {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerManagement.class, args);
+        System.out.println("this is test");
+    }
+    @GetMapping
+public String getMessage(){
+        return "Hello client";
+}
+}
